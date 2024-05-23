cab798cc61c961889f1b7613293e5c3b62da1393
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-05-23 22:07:45 +0200

    Add algorithm to reverse string

---
+++ algorithms/reverse_string.py
@@ -0,0 +1,17 @@
+def reverse_string(s):
+    if (len(s) == 0 or s == None):
+        return ''
+
+    chars = [c for c in s]
+    start = 0
+    end = len(chars) - 1
+
+    while start < end:
+        chars[start], chars[end] = chars[end], chars[start]
+        start += 1
+        end -= 1
+
+    return ''.join(chars)
+
+s = 'Hello, World!'
+print(reverse_string(s))
