2430669a15bcaf5b5d1e779257388716ea783e5f
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-05-23 22:22:42 +0200

    Add algorithm to check if array has pair which is equal to pair

---
+++ algorithms/has_pair_with_sum.py
@@ -0,0 +1,14 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def has_pair_with_sum(arr, sum):
+    s = set()
+
+    for i in range(0, len(arr)):
+        if arr[i] in s:
+            return True
+        else:
+            s.add(sum - arr[i])
+
+    return False
+
+print(has_pair_with_sum([3,2,1,5], 7))
