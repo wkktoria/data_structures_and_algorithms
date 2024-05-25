14a0350e1d9dc0acc87d327fbc59b597a7890cb4
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-05-25 21:14:55 +0200

    Add unit test for Contains Duplicate problem

--- algorithms/leetcode/contains_duplicate.py
+++ algorithms/leetcode/contains_duplicate.py
@@ -1,8 +1,23 @@
+import unittest
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def contains_duplicate(nums):
     s = set(nums)
     return len(s) != len(nums)
 
 
-print(contains_duplicate([1, 2, 3, 1]))
-print(contains_duplicate([1, 2, 3, 4]))
-print(contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+class TestContainsDuplicate(unittest.TestCase):
+    def test_example1(self):
+        self.assertEqual(contains_duplicate([1, 2, 3, 1]), True)
+
+    def test_example2(self):
+        self.assertEqual(contains_duplicate([1, 2, 3, 4]), False)
+
+    def test_example3(self):
+        self.assertEqual(contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
